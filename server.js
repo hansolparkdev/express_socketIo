f6d6fbb5c6fee184fecd6148515baba048746231
hansolparkdev <solidev90@gmail.com>
2019-04-30 20:39:43 +0900

    socket.io chat Example v.0.0.1

--- server.js
+++ server.js
@@ -9,9 +9,37 @@ app.get('/', function(req, res){
 })
 
 io.on('connection', function(socket){
-   console.log("connection")
-})
 
+   socket.on('login', function(data){
+     console.log('client logged-in:\n name : ' + data.name + '\n userid : ' + data.userid);
+
+     socket.name = data.name;
+     socket.userid = data.userid;
+
+     io.emit('login', data.name);
+   });
+
+
+   socket.on('chat', function(data){
+     console.log('Message from %s: %s', socket.name, data.msg);
+
+     var msg = {
+       from : {
+         name : socket.name,
+         userid : socket.userid
+       },
+       msg : data.msg
+     };
+
+     socket.broadcast.emit('chat', msg);
+   });
+   socket.on('forceDisconnect', function(){
+     socket.disconnect();
+   })
+   socket.on('disconnect', function(){
+     console.log('user disconnected : ' + socket.name);
+   })
+});
 server.listen(3000, function(){
   console.log(
     `
